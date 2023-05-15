18ed3a6ac8746aa49d531ed8a480ea790a1c8910
Nikhil Awatade <nikhilawatade13@gmail.com>
2023-05-15 11:25:39 +0530

    final

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ from os import path
 import subprocess
 import pandas
 import requests
-
+import moviepy.editor as mp
+import shutil
 
 r = sr.Recognizer()
 
@@ -23,41 +24,28 @@ def get_large_audio_transcription(path):
     Splitting the large audio file into chunks
     and apply speech recognition on each of these chunks
     """
-    # open the audio file using pydub
     sound = AudioSegment.from_wav(path)
-    # split audio sound where silence is 700 miliseconds or more and get chunks
     chunks = split_on_silence(sound,
-                              # experiment with this value for your target audio file
                               min_silence_len=500,
-                              # adjust this per requirement
                               silence_thresh=sound.dBFS-14,
-                              # keep the silence for 1 second, adjustable as well
                               keep_silence=500,
                               )
     folder_name = "audio-chunks"
-    # create a directory to store the audio chunks
     if not os.path.isdir(folder_name):
         os.mkdir(folder_name)
     whole_text = ""
-    # process each chunk
     for i, audio_chunk in enumerate(chunks, start=1):
-        # export audio chunk and save it in
-        # the `folder_name` directory.
         chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
         audio_chunk.export(chunk_filename, format="wav")
-        # recognize the chunk
         with sr.AudioFile(chunk_filename) as source:
             audio_listened = r.record(source)
-            # try converting it to text
             try:
                 text = r.recognize_google(audio_listened)
             except sr.UnknownValueError as e:
                 print("Error:", str(e))
             else:
                 text = f"{text.capitalize()}. "
-                # print(chunk_filename, ":", text)
                 whole_text += text
-    # return the text for all chunks detected
     return whole_text
 
 
@@ -84,8 +72,25 @@ def home():
         file = form.file.data  # First grab the file
         file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),
                   app.config['UPLOAD_FOLDER'], secure_filename("test.wav")))  # Then save the file
-        return render_template('index1.html', form=form, result=get_large_audio_transcription(file))
+        # print(type(file))
+        return render_template('index1.html', form=form, result=get_large_audio_transcription(file),st=0)
     return render_template('index.html', form=form, result="")
+
+@app.route('/video',methods=['GET', "POST"])
+def video():
+    form=UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                  app.config['UPLOAD_FOLDE'], secure_filename("sample.mp4")))
+        clip = mp.VideoFileClip("sample.mp4")
+        clip.audio.write_audiofile("test.wav")
+        shutil.move(os.path.join("", "test.wav"), os.path.join("F:\\Desktop\\speech_recognition\\static", "test.wav"))
+        shutil.copy2(os.path.join("", "sample.mp4"), os.path.join("F:\\Desktop\\speech_recognition\\static", "sample.mp4"))
+        return render_template('index1.html', form=form, result=get_large_audio_transcription("F:\\Desktop\\speech_recognition\\static\\test.wav"),st=1)
+    return render_template('index.html', form=form, result="")
+        
+    
 
 
 #hello
